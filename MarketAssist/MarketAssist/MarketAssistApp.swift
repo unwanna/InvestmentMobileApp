f40484169ec98e2f3f565f914fad653844f13d17
Uzoma Nwanna <uzoma.nwanna@sofiac.us>
2021-11-26 20:06:43 -0500

    Initial commit

---
+++ MarketAssist/MarketAssist/MarketAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketAssistApp.swift
+//  MarketAssist
+//
+//  Created by Uzoma Nwanna on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MarketAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
